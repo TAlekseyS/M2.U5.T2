6074fe0422509b233d8680ee3118273103006d2c
Aleksey_T <talekseys@mail.ru>
2023-08-10 13:52:04 +0300

    Perepisaal

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/b3i331h25wlurkci?svg=true)](https://ci.appveyor.com/project/TAlekseyS/m2-u5-t2)
--- src/test/java/TestAppIbank.java
+++ src/test/java/TestAppIbank.java
@@ -1,5 +1,34 @@
+import com.codeborne.selenide.Condition;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static data.DataGenerator.Registration.getRegisteredUser;
+import static io.restassured.RestAssured.given;
+
 public class TestAppIbank {
+    // спецификация нужна для того, чтобы переиспользовать настройки в разных запросах
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+
     @Test
+    @DisplayName("should succesfully login with active registered user")
+    void shouldTestingIbank() {
+        var registeredUser = getRegisteredUser("active");
+        $("[data-test-id='login'] input").setValue(registeredUser.getLogin());
+        $("[data-test-id='password'] input").setValue(registeredUser.getPassword());
+        $("button.button").click();
+        $("h2").shouldHave(Condition.exactText("Личный кабинет")).shouldBe(Condition.visible);
+    }
 }
+
